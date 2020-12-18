f7fdf0b8fef4b9eac086a3f2578e280aa3967886
Bachue Zhou <bachue.shu@gmail.com>
2020-12-18 10:22:40 +0800

    export Region & DEFAULT_UC_URL, bump version to 0.1.7

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { Qiniu, KODO_MODE, S3_MODE } from './qiniu';
+import { DEFAULT_UC_URL, Region } from './region';
 
 module.exports = {
-    Qiniu, KODO_MODE, S3_MODE
+    Qiniu, KODO_MODE, S3_MODE, DEFAULT_UC_URL, Region,
 };

6a6ded42bb8374361f10babdeb2177a7c0c0771d
Bachue Zhou <bachue.shu@gmail.com>
2021-01-06 13:34:14 +0800

    Bump version to 0.1.18

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { Qiniu, KODO_MODE, S3_MODE } from './qiniu';
 import { DEFAULT_UC_URL, Region } from './region';
 import { RegionService } from './region_service';
+import { Uploader } from './uploader';
 
 module.exports = {
-    Qiniu, KODO_MODE, S3_MODE, DEFAULT_UC_URL, Region, RegionService,
+    Qiniu, KODO_MODE, S3_MODE, DEFAULT_UC_URL, Region, RegionService, Uploader,
 };
